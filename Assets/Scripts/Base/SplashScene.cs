f0df37d3cf42d174d876e02632a8e016b66a84a9
thinhvu1205 <thinhvu120593@gmail.com>
2026-03-02 11:15:15 +0700

    1

--- Assets/Scripts/Base/SplashScene.cs
+++ Assets/Scripts/Base/SplashScene.cs
@@ -12,7 +12,7 @@ public class SplashScene : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         string storedUrl = PlayerPrefs.GetString(BundleDownloader.STORED_BUNDLE_URL, "https://storage.googleapis.com/tongitswar/AssetBundles");
-        storedUrl = "D:/Unity projects/Tidi-Phil-Win777/Assets/AssetBundles";
+        // storedUrl = "D:/Unity projects/Tidi-Phil-Win777/Assets/AssetBundles";
         m_BundleBD.CheckAndDownloadAssets(storedUrl, 1f,
             () =>
             {
